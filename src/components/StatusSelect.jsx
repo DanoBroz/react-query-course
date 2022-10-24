f5d7aa2bcd6af01677c9354f31743953414a8cf3
Daniel Brož <danobroz66@gmail.com>
2022-10-24 08:06:30 +0200

    create status selector with possible statuses

---
+++ src/components/StatusSelect.jsx
@@ -0,0 +1,35 @@
+const possibleStatus = [
+  {
+    id: "backlog",
+    label: "Backlog",
+  },
+  {
+    id: "todo",
+    label: "To-do",
+  },
+  {
+    id: "inProgress",
+    label: "In Progress",
+  },
+  {
+    id: "done",
+    label: "Done",
+  },
+  {
+    id: "canceled",
+    label: "Cancelled",
+  },
+];
+
+export function StatusSelect({ value, onChange }) {
+  return (
+    <select value={value} onChange={onChange} className={"status-select"}>
+      <option value={""}>Select status to filter</option>
+      {possibleStatus.map((status) => (
+        <option key={status.id} value={status.id}>
+          {status.label}
+        </option>
+      ))}
+    </select>
+  );
+}
