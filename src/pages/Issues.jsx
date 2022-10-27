c4b411c4bc541806bce20aada8ba84fad1d76586
Daniel Brož <danobroz66@gmail.com>
2022-10-27 14:26:57 +0200

    create and use paginated queries for issues

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -7,31 +7,41 @@ import { Link } from "react-router-dom";
 export default function Issues() {
   const [labels, setLabels] = useState([]);
   const [status, setStatus] = useState("");
+  const [pageNum, setPageNum] = useState(1);
 
   return (
     <div>
       <main>
         <section>
           <h1>Issues</h1>
-          <IssuesList labels={labels} status={status} />
+          <IssuesList
+            labels={labels}
+            status={status}
+            pageNum={pageNum}
+            setPageNum={setPageNum}
+          />
         </section>
         <aside>
           <LabelList
             selected={labels}
-            toggle={(label) =>
+            toggle={(label) => {
               setLabels((currentLabels) =>
                 currentLabels.includes(label)
                   ? currentLabels.filter(
                       (currentLabel) => currentLabel !== label
                     )
                   : currentLabels.concat(label)
-              )
-            }
+              );
+              setPageNum(1);
+            }}
           />
           <h3>Status</h3>
           <StatusSelect
             value={status}
-            onChange={(e) => setStatus(e.target.value)}
+            onChange={(e) => {
+              setStatus(e.target.value);
+              setPageNum(1);
+            }}
           />
           <hr />
           <Link className="button" to="/add">
